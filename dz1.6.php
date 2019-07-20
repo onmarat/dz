7355dcc1be9f9b9908194b246556fbea9449fdf3
Marat <onmarat@gmail.com>
2019-07-20 21:01:51 +0300

    6 коммит

---
+++ dz1.6.php
@@ -0,0 +1,21 @@
+<table>
+<?php
+
+for ($i = 1; $i <=10; $i++) {
+    print "<tr>";
+  for ($b = 1; $b >= 10; $b++) {
+      $n = $i * $b;
+      print "<td>";
+      if ($n%2 ==0) {
+          print "(".$n.")";
+      }
+      else {
+          print "[".$n."]";
+      }
+      print "</td>";
+  }
+  print "</tr>";
+}
+
+?>
+</table>
