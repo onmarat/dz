80a8dcda0c25f97312f81887bd005fd9ae48b3af
Marat <onmarat@gmail.com>
2019-07-20 20:28:06 +0300

    4 коммит

---
+++ dz1.4.php
@@ -0,0 +1,34 @@
+<?php
+
+$day = 1;
+
+
+$budn = "Это рабочий день";
+$holid = "Это выходной день";
+
+switch ($day) {
+    case 1:
+        echo $budn;
+        break;
+    case 2:
+        echo $budn;
+        break;
+    case 3:
+        echo $budn;
+        break;
+    case 4:
+        echo $budn;
+        break;
+    case 5:
+        echo $budn;
+        break;
+    case 6:
+        echo $holid;
+        break;
+    case 7:
+        echo $holid;
+        break;
+    default:
+        echo "Неизвестный день";
+        break;
+}
