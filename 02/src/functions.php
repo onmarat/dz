b297dd1b55bc6d7d2761933d27b6f19616552f43
Marat <onmarat@gmail.com>
2019-07-28 11:06:02 +0300

    Функция task2

--- 02/src/functions.php
+++ 02/src/functions.php
@@ -14,10 +14,23 @@ function task1 ($p1, $p2) {
 }
 
 function task2 ($p1) {
-
-
-
-
-
+    $p2 = func_get_args();
+    $i = 1;
+    while ($i < sizeof($p2)){
+        if ($p1 == "+") {
+            $n = $p2[$i] + $p2[$i+1];
+        }
+        elseif ($p1 == "-"){
+            $n = $p2[$i] - $p2[$i+1];
+        }
+        elseif ($p1 == "*"){
+            $n = $p2[$i] * $p2[$i+1];
+        }
+        else {
+            $n = false;
+        }
+        $i++;
+    }
+    return $n;
 
 }
