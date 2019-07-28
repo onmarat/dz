9f35d3e07a4ec7367a000605279681990d46181c
Marat <onmarat@gmail.com>
2019-07-28 11:44:25 +0300

    Функция task5

--- 02/index.php
+++ 02/index.php
@@ -8,3 +8,12 @@ print date('d.m.Y h:i:s');
 
 $datetime1 = strtotime('2016-02-24 00:00:00');
 print $datetime1;
+
+
+//task 5
+
+$nn = "Карл у Клары украл Кораллы";
+print str_replace("К", "", $nn);
+
+$bb = "Две бутылки лимонада";
+print str_replace("Две", "Три", $bb);
